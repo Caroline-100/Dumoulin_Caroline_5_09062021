f4bda3c58c308b3f2b17ed1ab0b592e6c8383c80
Caroline <Dumoulin>
2021-08-31 14:55:42 +0200

    test + change page product (comment)

--- front/product.js
+++ front/product.js
@@ -52,16 +52,7 @@ fetch(`http://localhost:3000/api/teddies/${teddyId}`)
     for (let color of teddies.colors) {
       const colorName = color[0];
       const colorCode = color[1];
-      // console.log(buttonsColorPlush);
-      //   buttonsColorPlush += `<a href="./panier.html">
-      //       <button class= "${colorName}" style=
-      // "background-color: ${colorCode}; "
-      // >${colorName}</button>
-      // </a>`;
-      // }
 
-      // for (let color of teddies.colors) {
-      // colorName = color[0];
       let buttonsColor = document.querySelector(`.${colorName}`);
       console.log(buttonsColor);
       // setting up the buttons in an html element present on the html page
@@ -73,8 +64,6 @@ fetch(`http://localhost:3000/api/teddies/${teddyId}`)
         // i retrieve the value in localStorage, the array empty
         valuelocalColor = localStorage.getItem("color");
       }
-
-      // console.log(valuelocalColor);
 
       // in the localstorage the values are in format string, i change for get an array of colors
       let arrayStorecolors = JSON.parse(valuelocalColor);
@@ -94,80 +83,6 @@ fetch(`http://localhost:3000/api/teddies/${teddyId}`)
         elementArticleBasket.textContent = localStorage.getItem("number");
       });
     }
-
-    // for (let [index, color] of teddies.colors.entries()) {
-    //   const colorName = color[0];
-    //   const colorCode = color[1];
-
-    //   buttonsColorPlush += `<a href="./panier.html">
-    //     <button class= "${colorName}" style=
-    //     "background-color: ${colorCode}; "
-    //     >${colorName}</button>
-    //     </a>`;
-    //   elementSquareColors.innerHTML = buttonsColorPlush;
-
-    //   let buttonsColor = document.querySelector(`.${colorName}`);
-    //   console.log(buttonsColor);
-    //   // setting up the buttons in an html element present on the html page
-    //   // in the localStorage i retrieve the old value
-    //   let valuelocalColor = localStorage.getItem("color");
-    //   // if the localstorage return null i add an empty array
-    //   if (valuelocalColor === null) {
-    //     localStorage.setItem("color", JSON.stringify([]));
-    //     // i retrieve the value in localStorage, the array empty
-    //     valuelocalColor = localStorage.getItem("color");
-    //   }
-    //   // in the localstorage the values are in format string, i change for get an array of colors
-    //   let arrayStorecolors = JSON.parse(valuelocalColor);
-
-    //   buttonsColor.addEventListener("click", (event) => {
-    //     // classname is the name of colors
-    //     let currentItem = event.target.className;
-    //     // i add old values with currentvalue in array empty
-    //     arrayStorecolors.push([currentItem, `${teddyId}`, teddies.price]);
-    //     // in the localStorage, i update the array whose content old value and currrent value
-    //     localStorage.setItem("color", JSON.stringify(arrayStorecolors));
-
-    //     localStorage.setItem("number", arrayStorecolors.length);
-    //     let elementArticleBasket = document.querySelector(
-    //       "#number_element_basket"
-    //     );
-    //     // i display the current number of article in the basket
-    //     elementArticleBasket.textContent = localStorage.getItem("number");
-    //   });
-    //   console.log(arrayStorecolors);
-    // }
-    // console.log(elementSquareColors);
-    // console.log(buttonsColorPlush);
-    // i loop over the array of colors of API which returns the numbers of colors for each teddies
-
-    // for (let numbersColorsByTeddy in teddies.colors) {
-    //   // i retrieve only the name of colors
-    //   buttonsColor = document.querySelector(
-    //     `.${teddies.colors[numbersColorsByTeddy][0]}`
-    //   );
-    //   console.log(`.${teddies.colors[numbersColorsByTeddy][0]}`);
-    //   let elementArticleBasket = document.querySelector(
-    //     "#number_element_basket"
-    //   );
-    //   elementArticleBasket.textContent = localStorage.getItem("number");
-    //   // i create an event on button of colors, when the buttons is clicked i add in the localstorage the current color and id product
-    //   // buttonsColor.addEventListener("click", (event) => {
-    //   //   // classname is the name of colors
-    //   //   let currentItem = event.target.className;
-    //   //   // i add old values with currentvalue in array empty
-    //   //   arrayStorecolors.push([currentItem, `${teddyId}`, teddies.price]);
-    //   //   // in the localStorage, i update the array whose content old value and currrent value
-    //   //   localStorage.setItem("color", JSON.stringify(arrayStorecolors));
-
-    //   //   localStorage.setItem("number", arrayStorecolors.length);
-    //   //   let elementArticleBasket = document.querySelector(
-    //   //     "#number_element_basket"
-    //   //   );
-    //   //   // i display the current number of article in the basket
-    //   //   elementArticleBasket.textContent = localStorage.getItem("number");
-    //   // });
-    // }
   })
   .catch((error) => {
     console.error(error);
